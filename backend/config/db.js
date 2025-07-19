cf9f1fd518c3f1e1579372b6bdf9cf8672a12a68
unknown <saqibmir580@gmail.com>
2025-07-19 21:18:27 +0530

    mogodb issue

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect(process.env.MONGODB_URI {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("MongoDB Connected");
   } catch (err) {
     console.error(err.message);
